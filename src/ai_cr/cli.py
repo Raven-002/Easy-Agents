8343b1a272b86e9086d568801a1dd2868904218b
Raven <152022920+Raven-002@users.noreply.github.com>
2025-12-22 21:31:56 +0200

    Revert "Add missing files"
    This reverts commit 22f6bd801fb8dbfc6e4250c2747ed92bdd1d9f49.

--- src/ai_cr/cli.py
+++ src/ai_cr/cli.py
@@ -1,16 +1,19 @@
+"""Console script for ai_cr."""
+
 import typer
 from rich.console import Console
 
-from easy_agents.agents.cr_agent import generate_code_review
-from easy_agents.app import app
-from easy_agents.settings.settings import load_settings_from_yaml
-from easy_agents.utils.logging_utils import configure_logging, dlog, status
+from ai_cr.settings.settings import load_settings_from_yaml
 
+from .cr_generator import generate_code_review
+from .utils.logging_utils import configure_logging, dlog, status
+
+app = typer.Typer()
 console = Console()
 
 
 @app.command()
-def perform_cr(
+def main(
     target_branch: str = typer.Argument(help="Target branch to generate code review for."),
     skip_print: bool = typer.Option(False, help="Skip printing generated code review."),
     cache_dir: str = typer.Option(".cr.cache", envvar="AI_CR_CACHE_DIR", help="Directory to store cache files."),
@@ -22,9 +25,7 @@ def perform_cr(
         "--spinner/--no-spinner",
         help="Show a spinner while waiting for the AI model response (default: auto, only in TTY).",
     ),
-    settings_file: str = typer.Option(
-        ".easy_agents_config.yml", envvar="AI_CR_SETTINGS_FILE", help="Path to settings file."
-    ),
+    settings_file: str = typer.Option(".ai_cr_config.yml", envvar="AI_CR_SETTINGS_FILE", help="Path to settings file."),
 ) -> None:
     """Perform code review for a given branch."""
     configure_logging(level=2 if debug else 1 if verbose else 0, console=console, spinner=spinner)
@@ -33,7 +34,7 @@ def perform_cr(
         load_settings_from_yaml(f.read())
 
     dlog(
-        f"easy_agents {target_branch}{' --skip-print' if skip_print else ''} "
+        f"ai_cr {target_branch}{' --skip-print' if skip_print else ''} "
         f"--cache-dir {cache_dir}{' --save-to-file ' + save_to_file if save_to_file else ''}"
     )
 
@@ -41,3 +42,7 @@ def perform_cr(
         cr = generate_code_review(target_branch)
     if not skip_print:
         cr.print_comments(console)
+
+
+if __name__ == "__main__":
+    app()
